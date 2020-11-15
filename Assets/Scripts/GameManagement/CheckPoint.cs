45b0ec156f38a4fc14c91e913c53cd2af7962338
atikana <pratana.atikhomkamalasai@mail.utoronto.ca>
2020-11-15 14:22:36 -0500

    add checkpoints

--- Assets/Scripts/GameManagement/CheckPoint.cs
+++ Assets/Scripts/GameManagement/CheckPoint.cs
@@ -5,6 +5,25 @@ public class CheckPoint : MonoBehaviour
     // Player's optimal rotation when starting at this checkpoint.
     public float playerRotation;
 
+    Transform front;
+    Transform back;
+    Color original;
+    Color neonGreen = new Color(7, 299, 3);
+
+
+
+    private void Awake()
+    {
+        front = transform.GetChild(0);
+        back = transform.GetChild(1);
+
+
+        if (name.CompareTo("1") == 0)
+        {
+            ChangeColor(neonGreen);
+        }
+    }
+
     public Vector3 GetCheckPointPosition()
     {
         return transform.position;
@@ -15,6 +34,20 @@ public class CheckPoint : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             transform.parent.GetComponent<CheckPointManager>().AddCheckPoint(this);
+            ChangeColor(neonGreen);
         }
     }
+
+    void ChangeColor(Color c)
+    {
+        front.GetComponent<Renderer>().material.color = c;
+        back.GetComponent<Renderer>().material.color = c;
+    }
+
+    public void RevertColor()
+    {
+        ChangeColor(original);
+    }
+
+    
 }
