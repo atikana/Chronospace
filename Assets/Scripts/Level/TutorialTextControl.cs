c905b6f11c9da03f9787d4e4f6b523fad58e94c5
atikana <pratana.atikhomkamalasai@mail.utoronto.ca>
2020-11-22 15:45:07 -0500

    add instruction to the tutorial

--- Assets/Scripts/Level/TutorialTextControl.cs
+++ Assets/Scripts/Level/TutorialTextControl.cs
@@ -4,14 +4,13 @@ using UnityEngine.UI;
 public class TutorialTextControl : MonoBehaviour
 {
     public Transform playerTransform;
-    private Text text;
-    private int textCounter = 0;
+
+    private GameObject text;
 
     // Start is called before the first frame update
     void Start()
     {
-        text = GetComponent<Text>();
-        text.text = "";
+
     }
 
     // Update is called once per frame
@@ -19,49 +18,45 @@ public class TutorialTextControl : MonoBehaviour
     {
         if (playerTransform.position.z > 70 && playerTransform.position.z < 95)
         {
-            text.text = "Left joystick to walk (Xbox) or WASD (keyboard) to use move";
-            textCounter = 300;
+            ShowText(0);
         }
         else if (playerTransform.position.z > 95 && playerTransform.position.z < 130)
         {
-            text.text = "Right joystick to look (Xbox) or Move the mouse to look (PC)";
-            textCounter = 300;
+            ShowText(1);
         }
         else if (playerTransform.position.z > 130 && playerTransform.position.z < 160)
         {
-            text.text = "Walk over the checkpoint to activate it";
-            textCounter = 300;
+            ShowText(2);
         }
-        else if (playerTransform.position.z > 160 && playerTransform.position.z < 240)
+        else if (playerTransform.position.z > 170 && playerTransform.position.z < 200)
         {
-            text.text = "Press X (Xbox) or Shift (keyboard) to use dash";
-            textCounter = 300;
+            ShowText(3);
         }
-        else if (playerTransform.position.z > 250 && playerTransform.position.z < 295)
+        else if (playerTransform.position.z > 200 && playerTransform.position.z < 250)
         {
-            text.text = "Press B (Xbox) or Q (keyboard) for time warp";
-            textCounter = 300;
+            ShowText(4);
         }
-        else if (playerTransform.position.z > 300 && playerTransform.position.z < 325)
+        else if (playerTransform.position.z > 250 && playerTransform.position.z < 300)
         {
-            text.text = "Hold RT (Xbox) or left-click (mouse) to shoot the grapple";
-            textCounter = 300;
+            ShowText(5);
         }
         else if (playerTransform.position.z > 400 && playerTransform.position.z < 440)
         {
-            text.text = "Hold LT or right-click to pull while grapple is attached";
-            textCounter = 300;
-        }
-        else
-        {
-            if (textCounter > 0)
-            {
-                textCounter--;
-            }
-            else
-            {
-                text.text = "";
-            }
+            ShowText(6);
         }
     }
+
+
+    private void ShowText(int index)
+    {
+        if (text != null && text.activeInHierarchy)
+        {
+            text.SetActive(false);
+        }
+        text = transform.GetChild(index).gameObject;
+        text.SetActive(true);
+    }
 }
+
+
+
