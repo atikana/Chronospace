fc19ac6067c71b48e8ced4f38e30b2742945811f
GKHalbert <jingwei.shang@mail.utoronto.ca>
2020-11-24 19:22:21 +0800

    All new color materials added; Building assets restructured; Building collider and script added.

---
+++ Assets/Scripts/Level/BuildingCollision.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingCollision : MonoBehaviour
+{
+    private GameManager gameManager;
+
+    private void Start()
+    {
+        gameManager = FindObjectOfType<GameManager>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        // If the player hits a level boundary, kill them.
+        if (collision.collider.tag == "Player")
+        {
+            gameManager.KillPlayer();
+        }
+    }
+}
