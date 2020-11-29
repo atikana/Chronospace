249a77447fef848515fc8bb9579c7b04f648f75e
atikana <pratana.atikhomkamalasai@mail.utoronto.ca>
2020-11-28 21:07:30 -0500

    fixed building colliders

--- Assets/Scripts/Level/BuildingCollision.cs
+++ Assets/Scripts/Level/BuildingCollision.cs
@@ -12,9 +12,9 @@ public class BuildingCollision : MonoBehaviour
     private void OnCollisionEnter(Collision collision)
     {
         // If the player hits a level boundary, kill them.
-        if (collision.collider.tag == "Player")
+       /* if (collision.collider.tag == "Player")
         {
             gameManager.KillPlayer();
-        }
+        } */
     }
 }
